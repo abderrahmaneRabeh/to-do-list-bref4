6cac991f7709761abcc92e8cbc00c3e9a061d047
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-10-31 15:12:46 +0100

    #3 terminer améliorer le code html et js et ajoute des petite style

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-// afficher formumaire
+let list_taches = [];
 
+
+// affichage de formulaire par click sur le bouton
 const btn_show_form = document.getElementById("btn-show-form");
 const form = document.querySelector("form");
 
 btn_show_form.addEventListener("click", () => {
+
   if (btn_show_form.classList.contains("open")) {
     form.style.display = "block";
     btn_show_form.classList.remove("open");
@@ -13,6 +16,7 @@ btn_show_form.addEventListener("click", () => {
     btn_show_form.classList.add("open");
     btn_show_form.innerHTML = "Ajouter une tâche";
   }
+
 });
 
 const btn_ajouter = document.getElementById("btn-add");
@@ -25,22 +29,21 @@ const description = document.getElementById("description");
 const date = document.getElementById("date");
 const priorite = document.getElementById("priority");
 const statut = document.getElementById("status");
+
 const show_error = document.getElementById("show-error");
+
 const box_todo = document.querySelector(".box-todo .taches");
 const box_doing = document.querySelector(".box-doing .taches");
 const box_done = document.querySelector(".box-done .taches");
+const no_taches = document.getElementById("no-taches");
 
-let list_taches = [];
 
-// if (localStorage.getItem("taches")) {
-//   list_taches = JSON.parse(localStorage.getItem("taches"));
-// }
 
-// afficher_taches(list_taches);
-
+// ajouter une tâche 
 btn_ajouter.addEventListener("click", (e) => {
   e.preventDefault();
 
+  // validation des inputs
   if (titre.value == "" || description.value == "" || date.value == "") {
     show_error.innerHTML = "Veuillez remplir tous les champs";
     return;
@@ -48,6 +51,18 @@ btn_ajouter.addEventListener("click", (e) => {
     show_error.innerHTML = "";
   }
 
+  if (titre.value.length < 3) {
+    show_error.innerHTML = "Le titre doit contenir au moins 3 caractères";
+    return;
+  }
+
+  if (description.value.length < 5) {
+    show_error.innerHTML = "La description doit contenir au moins 5 caractères";
+    return;
+  }
+
+
+  // crée une nouvelle tâche
   const tache = {
     titre: titre.value,
     description: description.value,
@@ -56,22 +71,27 @@ btn_ajouter.addEventListener("click", (e) => {
     statut: statut.value,
   };
 
+  // ajouter la tâche dans la liste
   list_taches.push(tache);
-  // localStorage.setItem("taches", JSON.stringify(list_taches));
 
+
+  // afficher les tâches
   afficher_taches(list_taches);
 
+  // vider les inputs
   titre.value = "";
   description.value = "";
   date.value = "";
 });
 
-//afficher tach
-
+//afficher tout les tâches
 function afficher_taches(list_taches) {
+
+  // vider les tâches avant de les afficher 
   box_todo.innerHTML = "";
   box_doing.innerHTML = "";
   box_done.innerHTML = "";
+
 
   list_taches.forEach((tache) => {
     const card_tache = document.createElement("div");
@@ -88,20 +108,16 @@ function afficher_taches(list_taches) {
       <p id="card-description" class="text-center">${tache.description}</p>
     </div>
     <div class="card-footer-tach d-flex justify-content-evenly">
-      <span id="card-priority" class="bg-white px-1 rounded-3">${
-        tache.priorite
+      <span id="card-priority" class="bg-white px-1 rounded-3">${tache.priorite
       }</span>
       <button class="border-0"><i class="fa-solid fa-trash text-black"></i></button>
       <select name="statut-tache" class="tach_status">
-        <option value="To-do" ${
-          tache.statut == "To-do" ? "selected" : ""
-        } >To-do</option>
-        <option value="Doing" ${
-          tache.statut == "Doing" ? "selected" : ""
-        }>Doing</option>
-        <option value="Done" ${
-          tache.statut == "Done" ? "selected" : ""
-        }>Done</option>
+        <option value="To-do" ${tache.statut == "To-do" ? "selected" : ""
+      } >To-do</option>
+        <option value="Doing" ${tache.statut == "Doing" ? "selected" : ""
+      }>Doing</option>
+        <option value="Done" ${tache.statut == "Done" ? "selected" : ""
+      }>Done</option>
       </select>
       
     </div>
@@ -132,8 +148,14 @@ function afficher_taches(list_taches) {
 const nb_taches = document.getElementById("nb_taches");
 
 function total_taches() {
-  const taches = document.querySelectorAll(".card-tache");
-  nb_taches.innerHTML = taches.length;
+  const taches_numbers = document.querySelectorAll(".card-tache");
+  nb_taches.innerHTML = taches_numbers.length;
+
+  if (taches_numbers.length == 0) {
+    no_taches.style.display = "block";
+  } else {
+    no_taches.style.display = "none";
+  }
 }
 
 total_taches();
@@ -149,24 +171,7 @@ taches.addEventListener("click", (e) => {
   }
 });
 
-// chage de statut in tache and change place
-
-// const chnage_place_by_status = document.querySelectorAll(".tach_status");
-
-// chnage_place_by_status.forEach((status) => {
-//   status.addEventListener("change", (e) => {
-//     const tache = e.target.closest(".card-tache");
-//     if (e.target.value == "To-do") {
-//       box_todo.appendChild(tache);
-//     } else if (e.target.value == "Doing") {
-//       box_doing.appendChild(tache);
-//     } else if (e.target.value == "Done") {
-//       box_done.appendChild(tache);
-//     }
-//   });
-// });
-
-// Change of status and change place using event delegation
+// changer le statut de la tâche et la mettre dans la bonne box
 taches.addEventListener("change", (e) => {
   if (e.target.classList.contains("tach_status")) {
     const tache = e.target.closest(".card-tache");
